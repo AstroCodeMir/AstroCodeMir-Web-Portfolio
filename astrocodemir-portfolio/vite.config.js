74ae3718e835e2a0c47d8233b0f2da0972dc93b3
francine028 <franzaynfamorcan@gmail.com>
2025-09-26 20:59:16 +0800

    fix: configure the vite.config.js

--- astrocodemir-portfolio/vite.config.js
+++ astrocodemir-portfolio/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        portfolio: resolve (__dirname, "portfolio.html"),
         about: resolve(__dirname, "about.html"),
       },
     },
